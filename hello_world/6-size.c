451b14aac34d17d8ba96b508865c74fdf45533a8
cedricswing <7526@holbertonstudents.com>
2023-10-14 09:35:24 -0700

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,1 +1,17 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:(0)
+ * Description last project answer
+int main() {
+    
+    printf(" - char: %zu octets\n", sizeof(char));
+    printf(" - short: %zu octets\n", sizeof(short));
+    printf(" - int: %zu octets\n", sizeof(int));
+    printf(" - long: %zu octets\n", sizeof(long));
+    printf(" - long long: %zu octets\n", sizeof(long long));
+    printf(" - float: %zu octets\n", sizeof(float));
+    printf(" - double: %zu octets\n", sizeof(double));
+    printf(" - long double: %zu octets\n", sizeof(long double));
