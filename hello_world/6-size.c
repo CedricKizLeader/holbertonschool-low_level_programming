ea3b193600afeabe40fc0b8cb09e1ad55bec9ae9
cedricswing <7526@holbertonstudents.com>
2023-10-14 10:02:45 -0700

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-/*:
+/**
  * main - Entry point
  * Return: always 0 (sucess)
  * Description last project answer
  * */
+
 int main(void) 
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
