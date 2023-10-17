b96b87db8cf610a7d1b5522945795c0739f9fd67
cedricswing <7526@holbertonstudents.com>
2023-10-17 08:49:26 -0700

    100-print_comb3.c

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Return: Always 0 (Sucess)
+* Description: 01, 02, etc
+*
+*/
+	int main(void)
+	{
+	int n1, n2;
+
+	for (n1 = '0' ; n1 <= '9' ; n1++)
+	for (n2 = '1' ; n2 <= '9' ; n2++)
+	if (n1 != n2)
+	if (n1 < n2)
+	{
+	putchar (n1);
+	putchar (n2);
+	if (n1 != '8')
+	{
+	putchar (',');
+	putchar (' ');
+	}
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+	}
