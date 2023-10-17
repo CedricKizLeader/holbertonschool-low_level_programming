d88ef469e44e8bde3418e5393e152110e1b1edf6
cedricswing <7526@holbertonstudents.com>
2023-10-17 07:52:33 -0700

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * Description: Alphabet
+ */
+
+/** betty main Alphabet  minu */
+int main(void)
+
+{
+	putchar(0x30);
+	for (int i = 1; i < 10; i++)
+	{
+		putchar(0x2C);
+		putchar(0x20);
+		putchar(0x30 + i);
+	}
+	return (0);
+}
