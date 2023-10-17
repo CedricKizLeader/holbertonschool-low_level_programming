877ab6fe67a4d717a26f85d03e6f8d9d43ebb2bd
cedricswing <7526@holbertonstudents.com>
2023-10-17 08:19:04 -0700

    mmo

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- * Description: Alphabet
- */
-
-/** betty main Patience */
-
+main - Entry point
+Description: Prints number
+Return: Always 0 (Success)
+*/
 int main(void)
-
-	int i = 0;
 {
-	putchar('0');
+    int i = '0';
 
-	while (i < 9)
-	{
-		putchar(',');
-		putchar(' ');
-	}
-	{
-		putchar(i + '0');
-		putchar('\n');
-	}
-	return (0);
+    while (i <= '9')
+    {
+    putchar(i);
+    if (i != '9')
+    {
+    putchar(',');
+    putchar(' ');
+    }
+    ++i;
+    }
+    putchar('\n');
+    return (0);
 }
