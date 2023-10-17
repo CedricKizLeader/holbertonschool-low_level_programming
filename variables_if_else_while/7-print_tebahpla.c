39ac96f33917bdabb045d47bfa42bba9259e31a9
cedricswing <7526@holbertonstudents.com>
2023-10-17 07:09:13 -0700

    ..

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * Description: Alphabet
+ */
+
+/** betty main Alphabet  minu z-a */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+	{
+		putchar(letter);
+	}
+	{
+		putchar('\n');
+		}
+	return (0);
+}
