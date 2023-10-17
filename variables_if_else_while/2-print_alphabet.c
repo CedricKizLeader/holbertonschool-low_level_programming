76ba100aaca84d82e617c2cac1a6b911095de107
cedricswing <7526@holbertonstudents.com>
2023-10-17 04:13:39 -0700

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * Description: Last digit
+ */
+
+/** betty main Alphabet  minu */
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+
+		putchar(c);
+	{
+	putchar('n');
+	}
+	return (0);
+}
