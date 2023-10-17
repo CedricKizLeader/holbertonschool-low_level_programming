a9ad1ce2e8c843850ce0f1440ab314c72cf24edb
cedricswing <7526@holbertonstudents.com>
2023-10-17 08:22:27 -0700

    project1valided

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 /**
-main - Entry point
-Description: Prints number
-Return: Always 0 (Success)
+* main - Entry point
+* Description: Prints number
+* Return: Always 0 (Success)
 */
 int main(void)
 {
-    int i = '0';
+	int i = '0';
 
-    while (i <= '9')
-    {
-    putchar(i);
-    if (i != '9')
-    {
-    putchar(',');
-    putchar(' ');
-    }
-    ++i;
-    }
-    putchar('\n');
-    return (0);
+	while (i <= '9')
+	{
+		putchar(i);
+	if (i != '9')
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	++i;
+	}
+	putchar('\n');
+	return (0);
 }
