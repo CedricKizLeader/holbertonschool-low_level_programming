2986e5907c6d366abc3a43da3e0e05cc928d54de
cedricswing <7526@holbertonstudents.com>
2023-10-16 23:13:57 -0700

    n =<>

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,18 +4,33 @@
 
 
 /**
- * main -
+ * main - Entry point
  * Return: Always 0 (Success)
  * Description: rand
  */
+
 int main(void)
 {
 	int n;
+	int lastdigitof;
 
 	srand(time(0));
+
 	n = rand() - RAND_MAX / 2;
 
-
+	lastdigitof = (n % 10);
+	if (lastdigitof > 5)
+	{
+		printf("lastdigitof %d is greater than 5\n", n);
+	}
+	if (lastdigitof == 0)
+	{
+		 printf("lastdigitof %d is 0\n", n);
+	}
+	if (lastdigitof < 6 && lastdigitof != 0)
+	{
+		printf("lastdigifof is %d is les than 6 and not 0\n", n);
+	}
 
 	return (0);
 }
