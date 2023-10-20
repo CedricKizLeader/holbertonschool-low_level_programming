0d391844994bc39af270d648ed7c14129372ea2e
cedricswing <7526@holbertonstudents.com>
2023-10-19 23:38:55 -0700

    _Putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - _putchar
+ * Description : Write a progrzm that prints _putchar
+ * Return: Always 0.
+ */
+int main(void)
+
+
+{
+	printf("_Putchar");
+	putchar('\n');
+
+	return (0);
+}
