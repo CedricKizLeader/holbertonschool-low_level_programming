b61419c02ff5a59def62c0a9f669765264e934bc
cedricswing <7526@holbertonstudents.com>
2023-10-20 07:11:50 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints alphabet
- * print_alphabet: x10
+ * print_alphabet_x10 - Prints alphabet
+ * print_alphabet_x10 : x10
  * Return : Always 0 (Sucess)
  */
 
