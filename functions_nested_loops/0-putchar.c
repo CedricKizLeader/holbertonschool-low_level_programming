8f28e79de2ed35f1da54f11b671f58ad943b6374
cedricswing <7526@holbertonstudents.com>
2023-10-20 02:16:21 -0700

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - _putchar
+ * imain - _putchar
  * Description : Write a progrzm that prints _putchar
  * Return: Always 0.
  */
 int main(void)
-
-	char text[] = ["_Putchar"];
 {
-	putchar('[_]Putchar');
-	putchar('\n');
+	char text[] = "_Putchar";
+	int i = 0;
+	int size = sizeof(text) - 1;
 
+	while (i < size);
+
+	{
+		putchar(text[i]);
+		write(1, &text[i], 1),
+		i++;
+	}
 	return (0);
 }
