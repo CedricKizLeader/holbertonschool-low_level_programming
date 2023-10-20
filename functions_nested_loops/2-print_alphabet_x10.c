6598807455c4f4dab3e38fa20437ac736b776025
cedricswing <7526@holbertonstudents.com>
2023-10-20 06:06:29 -0700

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_alphabet_x10 - jhftutrut
+ * return : o.
+ */
+
+void print_alphabet_x10(void);
+	char letter;
+
+	for (int i = 0; i < 10; i++)
+	{
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+
+	_putchar('\n');
+	}
