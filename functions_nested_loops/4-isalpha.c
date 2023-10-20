57714f1c6e5a7dc568a5d3d666ec8dc6d023a928
cedricswing <7526@holbertonstudents.com>
2023-10-20 07:28:26 -0700

    4-isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isalpha - returns 1 if character is lowercase else 0
+* _isalpha A or 1
+* Return: Always 0 (Success)
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
