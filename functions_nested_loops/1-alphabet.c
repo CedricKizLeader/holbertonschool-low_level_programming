8a6075027af0986b10672a18eec7a752653185d8
cedricswing <7526@holbertonstudents.com>
2023-10-20 05:11:52 -0700

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,21 +1,22 @@
+#include "main.h"
 #include <stdio.h>
+
 /**
  * main - Entry point
- * Return: Always 0 (Success)
- * Description: Alphabet
+ * print_alphabet - jhftutrut
+ * print_alphabet -yoooyo
  */
 
-/** betty main Alphabet */
-int main(void)
+void print_alphabet(void);
+
 {
-	char letter;
+		char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		putchar(letter);
+		_putchar(letter);
 	}
 	{
-		putchar('\n');
-		}
-	return (0);
+		_putchar('\n')
+	}
 }
