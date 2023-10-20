c5ade8cf82adefa8d9c47a75956a31061151a0fe
cedricswing <7526@holbertonstudents.com>
2023-10-20 06:31:32 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * print_alphabet_x10 - jhftutrut
+ * print_alphabet:_x10 - jhftutrut
  * return : o.
  */
 
 void print_alphabet_x10(void);
+{
 	char letter;
 
-	for (int i = 0; i < 10; i++)
+	while (int i = 0; i < 10; i++)
+
 	{
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
+		for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 	}
-
 	_putchar('\n');
-	}
+}
