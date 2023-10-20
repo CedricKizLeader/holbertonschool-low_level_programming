ac94f88cf83873bd644debd90e987da98fcddbac
cedricswing <7526@holbertonstudents.com>
2023-10-20 06:45:45 -0700

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,14 @@
 void print_alphabet_x10(void);
 {
 	char letter;
+	int i;
 
-	while (int i = 0; i < 10; i++)
-
-	{
 		for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+		{
+		for (int i = 0; i < 10; i++)
+		{
+			_putchar(letter);
 	}
 	_putchar('\n');
+}
 }
