1c3dfe1eec33b8fd98f2a08a189c86335dcc3707
cedricswing <7526@holbertonstudents.com>
2023-10-20 02:20:17 -0700

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -8,16 +8,9 @@
  */
 int main(void)
 {
-	char text[] = "_Putchar";
-	int i = 0;
-	int size = sizeof(text) - 1;
-
-	while (i < size);
-
 	{
-		putchar(text[i]);
-		write(1, &text[i], 1),
-		i++;
+	char text[] = "_Putchar\n";
+	write( 1, text, sizeof(text) - 1);
 	}
 	return (0);
 }
