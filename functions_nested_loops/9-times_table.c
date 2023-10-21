db6340331d89b107025b19df84dd3a1cf5a67655
cedricswing <7526@holbertonstudents.com>
2023-10-21 05:45:41 -0700

    9-times_table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - a function that prints the 9 times table, starting with 0.
+ * Return - void, no return expected
+ * PlD with fabien is broken !!
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0 ; a <= 9 ; a++)
+{
+	_putchar('0');
+	for (b = 1; b <= 9; b++)
+{
+	_putchar(',');
+	_putchar(' ');
+	c = a * b;
+	if (c <= 9)
+	_putchar(' ');
+	else
+	_putchar((c / 10) + '0');
+
+	_putchar((c % 10) + '0');
+}
+	_putchar('\n');
+}
+}
