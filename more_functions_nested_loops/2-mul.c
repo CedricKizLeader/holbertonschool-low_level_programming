2e4cb331856254949026b3ea1734236ba6ff1c02
cedricswing <7526@holbertonstudents.com>
2023-10-21 14:25:24 -0700

    3-4 ??

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* mul - do a multiplication between two integers
+* @a: variable a
+* @b: variable b
+* Return: 1 if success, else 0.
+*/
+
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
