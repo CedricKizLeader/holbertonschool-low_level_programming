a7ba5c5a4a745fd3eeea2126675985d77e0b0b7e
cedricswing <7526@holbertonstudents.com>
2023-10-21 15:04:16 -0700

    SurrendMental

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_line - The shortest distance
+* @n: between two points is a straight line
+* Return: 1 if success, else 0.
+*/
+void print_line(int n)
+{
+	int c;
+
+	for (c = 0; c < n; c++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
