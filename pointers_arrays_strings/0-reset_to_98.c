0f4ea46f5e1ad161bf72bca9c7942735aa339662
cedricswing <7526@holbertonstudents.com>
2023-10-23 02:07:38 -0700

    warning

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- * int parameter and updates the value
- * Return: Always 0.
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value
+ * it points to to 98
+ * @n: pointer to an int
+ *
+ * Return: void
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
