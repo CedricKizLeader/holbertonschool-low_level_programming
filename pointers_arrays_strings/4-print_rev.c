12012d2ec601c266e740ee853eea4e23abd4c87f
cedricswing <7526@holbertonstudents.com>
2023-10-23 05:59:34 -0700

    rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse,followed by a ('\n')
+ * @s: vomisA caasI
+ * return: void
+ */
+
+void print_rev(char *s)
+
+{
+	int lenght = 0;
+
+	while (s[lenght] != '\0')
+	{
+		lenght++;
+	}
+	for (lenght - 1; lenght >= 0; lenght--)
+	{
+		_putchar(s[lenght]);
+	}
+	_putchar('\n');
+}
