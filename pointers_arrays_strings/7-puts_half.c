8d4c6a4e4c3cb1af4f014f78ba4ae898d6bc9a14
cedricswing <7526@holbertonstudents.com>
2023-10-25 02:10:58 -0700

    print other char of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: print the second half of the string
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int a = 0;
+	int b = 0;
+
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	if (a % 2 == 0)
+	{
+		b = a / 2;
+	}
+	else
+	{
+		b = (a +1) / 2;
+	}
+	while (str[b] != '\0')
+	{
+		_putchar(str[b]);
+		b++;
+	}
+	_putchar('\n');
+}
