5063ddfebd4349c9d0e8213545e6aa3491a53686
cedricswing <7526@holbertonstudents.com>
2023-10-28 08:40:26 -0700

    task1-2

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: the destination string
+ * @src: the source string
+ * @n: the number of src char to be printed
+ * Return: returns the concatenated string to *dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int destLen = 0;
+
+	while (dest[destLen] != '\0')
+	{
+	destLen++;
+	}
+	if (n != 0)
+	while (src[i] != '\0')
+	{
+	dest[destLen + i] = src[i];
+	i++;
+	if (i == n)
+		break;
+	}
+dest[destLen + i] = '\0';
+return (dest);
+}
---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string
+ * @dest: the destination of the string
+ * @src: the source of the string
+ * @n: number of byte to be copied
+ * Return: Returns the copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+	dest[i] = '\0';
+	i++;
+	}
+	return (dest);
+}
