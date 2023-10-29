67a85674db5675519438426867f100faafc272a1
cedricswing <7526@holbertonstudents.com>
2023-10-29 02:38:56 -0700

    lowercase rev uppercase

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * string_toupper - Transform loercases char into uppercases
+ * @ado: the string
+ * Return: returns the string in uppercase
+ */
+
+char *string_toupper(char *pls)
+{
+	int ff = 0;
+
+	while (pls[ff] != '\0')
+	{
+		if (pls[ff] >= 'a' && pls[ff] <= 'z')
+		{
+			pls[ff] -= 32;
+		}
+		ff++;
+	}
+	return (pls);
+}
