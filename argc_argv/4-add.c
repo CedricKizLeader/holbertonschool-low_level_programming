9fc9ba1637236ec180291bf2b228b94ebf38b8d8
cedricswing <7526@holbertonstudents.com>
2023-11-03 09:59:42 -0700

    endproject

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-
 /**
- * main - adds positive numbers.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * main - Entry point of the program
+ * @argc: The number of command line arguments
+ * @argv: An array of strings containing the arguments
+ *
+ * Return: 0 for success, 1 for error
  */
-
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
+	int i, result = 0;
 
-	for (i = 1; i < argc; i++)
-{
-	for (j = 0; argv[i][j] != '\0'; j++)
+	if (argc == 1)
 	{
-		if (!isdigit(argv[i][j]))
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] < '0' || *argv[i] > '9')
 		{
-			printf("error\n");
+			printf("Error\n");
 			return (1);
 		}
+		result += atoi(argv[i]);
 	}
-	add += atoi(argv[i]);
-}
-	printf("%d\n", add);
+	printf("%d\n", result);
 	return (0);
 }
