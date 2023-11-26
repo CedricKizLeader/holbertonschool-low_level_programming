f34ca56d72d7e0f64206253ce3191b7e485cf864
cedricswing <7526@holbertonstudents.com>
2023-11-26 08:54:32 -0800

    printf

---
+++ preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define MESSAGE "2-main.c"
+
+/**
+ * main - nom du fichier
+ * ecrit le nom de dossier
+ * Return: 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
