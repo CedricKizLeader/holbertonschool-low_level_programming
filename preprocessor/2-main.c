94048b319ca95f646c2a23676ed3822815c46819
cedricswing <7526@holbertonstudents.com>
2023-11-26 08:31:55 -0800

    TASK 0-1-2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define MESSAGE "2-main.c"
+
+/**
+ * main - MESSAGE
+ * ecrit le nom de dossier
+ * Return: 0.
+ */
+
+
+int main(void)
+
+{
+	puts(MESSAGE);
+	return (0);
+}
+
