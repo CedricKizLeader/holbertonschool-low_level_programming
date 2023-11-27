30d547d54981437ebcae6ff1884655a710982d2d
cedricswing <7526@holbertonstudents.com>
2023-11-27 04:59:21 -0800

    task0-main.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+
+/**
+ * print_list - Imprime tous les éléments d'une liste list_t.
+ * @h: Pointeur vers le début de la liste.
+ * Return: Le nombre de nœuds.
+ */
+
+size_t print_list(const list_t *h)
+
+{
+	const list_t *current = h;
+	size_t node_count = 0;
+
+	while (current != NULL)
+	{
+		printf("[%d] %s\n", current->len, current->str);
+		current = current->next;
+		node_count++;
+	}
+	return (node_count);
+}
