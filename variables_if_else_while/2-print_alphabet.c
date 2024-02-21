66432ae745b1156f6bcab5d4fa0617a783a109ea
CedricKizLeader <7526@holbertonstudents.com>
2024-02-21 02:16:31 -0800

    26_Letters

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -3,9 +3,12 @@
 /**
  * main - Affiche l'alphabet en minuscules
  *
- * Description: Ce programme utilise une boucle for pour itérer sur chaque lettre de l'alphabet en minuscules.
- * À chaque itération, la fonction putchar est utilisée pour afficher la lettre actuelle.
- * Après la boucle, putchar('\n') est utilisé pour ajouter une nouvelle ligne.
+ * Description: Ce programme utilise une boucle for pour itérer
+ * sur chaque lettre de l'alphabet en minuscules.
+ * À chaque itération, la fonction putchar est utilisée pour
+ * afficher la lettre actuelle.
+ * Après la boucle, putchar('\n') est utilisé pour
+ * :wqajouter une nouvelle ligne.
  * Return: Retourne 0 si le programme s'exécute correctement.
  */
 
