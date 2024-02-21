a4cc708923245cc30cc361f494ac318855aa7bc0
CedricKizLeader <7526@holbertonstudents.com>
2024-02-21 02:15:17 -0800

    26_Letters

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Affiche l'alphabet en minuscules
+ *
+ * Description: Ce programme utilise une boucle for pour itérer sur chaque lettre de l'alphabet en minuscules.
+ * À chaque itération, la fonction putchar est utilisée pour afficher la lettre actuelle.
+ * Après la boucle, putchar('\n') est utilisé pour ajouter une nouvelle ligne.
+ * Return: Retourne 0 si le programme s'exécute correctement.
+ */
+
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
