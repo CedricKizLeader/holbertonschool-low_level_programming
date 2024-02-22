7e52407e6f9c769798d49e410a73cc971ac32ce4
CedricKizLeader <7526@holbertonstudents.com>
2024-02-22 06:06:47 -0800

    Function putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main -_putchar writes the character c to stdout
+ *
+ * Return: On success 1.
+ */
+
+int main(void)
+
+{
+	printf("_putchar\n");
+	return (0);
+}
