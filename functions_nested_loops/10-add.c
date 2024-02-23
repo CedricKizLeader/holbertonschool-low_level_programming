8b440acf282583ba89f48ea1d4ab95fbc85cdcf4
CedricKizLeader <7526@holbertonstudents.com>
2024-02-23 05:49:30 -0800

    add number1 + number2, sum

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Additionne deux entiers.
+ * @number1: Le premier entier à ajouter.
+ * @number2: Le deuxième entier à ajouter.
+ *
+ * Return: La somme des deux entiers.
+ */
+
+
+
+int add(int number1, int number2)
+{	
+	int sum = (number1 + number2);
+
+		return (sum);
+}
