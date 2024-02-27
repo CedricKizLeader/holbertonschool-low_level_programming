22fff5e08a8c4bec312ee2e47709abeee38c76fd
CedricKizLeader <7526@holbertonstudents.com>
2024-02-27 14:13:25 +0000

    ImportMultipleMigraine

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Multiplie deux entiers.
+ * @a: Le premier entier à ajouter.
+ * @b: Le deuxième entier à ajouter.
+ *
+ * Return: La multiplication des deux entiers.
+ * dans le resultat migraine.
+ */
+
+int mul(int a, int b)
+
+{
+	int migraine = (a * b);
+		return (migraine);
+}
